e607b5906ea2a80e4d90357b292615374317b089
amor <amor.c@qq.com>
2016-11-11 10:51:55 +0800

    change 算法

--- passInit.php
+++ passInit.php
@@ -1,23 +1,9 @@
 <?php
-
+/**
+ * v0.1
+ */
 function generatePasswd($inputPasswdLength)
 {
-    /**
-     * v0.1
-     */
-
-//    $asciiList = array_merge(
-//        range(40,43),range(48,59),range(61,91),range(97,123)
-//    );
-//
-//    $keyList = array_rand($asciiList,$inputPasswdLength);
-//
-//    $str = "";
-//    foreach($keyList as $key)
-//    {
-//        $str .= chr($asciiList[$key]);
-//    }
-//    return $str;
     $str = "";
     for ($i=0;$i<$inputPasswdLength;$i++)
     {
